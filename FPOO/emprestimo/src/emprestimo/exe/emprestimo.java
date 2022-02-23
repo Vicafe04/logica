715b485ac39215fb0917f89e7b7f93daea0913c1
Vicafe04 <98469669+Vicafe04@users.noreply.github.com>
2022-02-23 11:23:29 -0300

    aula

---
+++ FPOO/emprestimo/src/emprestimo/exe/emprestimo.java
@@ -0,0 +1,21 @@
+package emprestimo.exe;
+
+import java.util.Scanner;
+
+public class emprestimo {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		double E, R, P, T;
+		System.out.println("Digite a quantia do emprestimo: ");
+		E = ler.nextDouble();
+		System.out.println("Digite sua renda mensal: ");
+		R = ler.nextDouble();
+		System.out.println("Digite a quantia de parcelas que voc� deseja");
+		P = ler.nextDouble();
+		T = R / P;
+		if()
+
+	}
+
+}
